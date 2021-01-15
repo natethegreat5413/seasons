1dddf9b78e047e35dcca9aa0333b0ca00e8c1bf6
Nathan Cowley <nathancowley12@gmail.com>
2021-01-15 11:18:30 -0700

    working on the seasons display component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			lat: null,
-			errorMessage: ''
-		};
-	}
+	// this is abreviated syntax so you don't have to use 'this' or the constructor.
+	state = { lat: null, errorMessage: '' };
 
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
@@ -18,16 +13,12 @@ class App extends React.Component {
 		);
 	}
 
-	componentDidUpdate() {
-		console.log('my component did update');
-	}
-
 	// React says we have to define render!!
 	render() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage}</div>;
 		} else if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		} else {
 			return <div>Loading . . . </div>;
 		}
