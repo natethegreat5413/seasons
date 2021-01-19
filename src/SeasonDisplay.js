ee4c1f4443d464b93c33fa3c67483bf1b7440653
Nathan Cowley <nathancowley12@gmail.com>
2021-01-19 14:12:12 -0700

    styling and text is now rendering depending on the weather

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+	summer: {
+		text: "Let's hit the beach!",
+		iconName: 'sun'
+	},
+	winter: {
+		text: 'Buuur it is so cold!',
+		iconName: 'snowflake'
+	}
+};
 
 // this function helps determine whether it is winter or summer in the northern and summer hemispheres based on the new Date().getMonth() javascript function.
 const getSeason = (lat, month) => {
@@ -11,15 +23,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
+	const { text, iconName } = seasonConfig[season]; // { text, iconName }
 	// JSX logic
-	const seasonsDisplayText = season === 'winter' ? "Burr, it's super cold out here" : "Let's hit the beach";
-	const icon = season === 'winter' ? 'snowflake' : 'sun';
+	// const seasonsDisplayText = season === 'winter' ? "Burr, it's super cold out here" : "Let's hit the beach";
+	// const icon = season === 'winter' ? 'snowflake' : 'sun';
 
 	return (
-		<div>
-			<i className={`${icon} icon`} />
-			<h1>{seasonsDisplayText}</h1>
-			<i className={`${icon} icon`} />
+		<div className={`season-display ${season}`}>
+			<i className={`${iconName} icon icon-left massive`} />
+			<h1>{text}</h1>
+			<i className={`${iconName} icon icon-right massive`} />
 		</div>
 	);
 };
