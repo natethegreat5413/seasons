271ded545d81a2b1dc105b130d3c1ee1ce91106a
Nathan Cowley <nathancowley12@gmail.com>
2021-01-18 20:26:15 -0700

    added icons to the page

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -13,10 +13,13 @@ const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
 	// JSX logic
 	const seasonsDisplayText = season === 'winter' ? "Burr, it's super cold out here" : "Let's hit the beach";
+	const icon = season === 'winter' ? 'snowflake' : 'sun';
 
 	return (
 		<div>
+			<i className={`${icon} icon`} />
 			<h1>{seasonsDisplayText}</h1>
+			<i className={`${icon} icon`} />
 		</div>
 	);
 };
